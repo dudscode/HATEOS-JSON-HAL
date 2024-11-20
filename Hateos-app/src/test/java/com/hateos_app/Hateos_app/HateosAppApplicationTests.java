7d3677f5d0f3eb9b07addfe075701ebf2629d541
florencionath <nathalia.pinto@sptech.school>
2024-11-20 20:44:41 -0300

    feat: init project

---
+++ Hateos-app/src/test/java/com/hateos_app/Hateos_app/HateosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hateos_app.Hateos_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
